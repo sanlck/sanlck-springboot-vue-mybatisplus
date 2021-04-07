a707fa9569386b17da828cda1a57199e96268eed
sanlck <pychaomiaomiao@163.com>
2021-04-07 11:00:12 +0800

    新建SpringBoot项目

---
+++ src/main/java/com/caelan/island/IslandApplication.java
@@ -0,0 +1,13 @@
+package com.caelan.island;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IslandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caelan/island/IslandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caelan.island;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
